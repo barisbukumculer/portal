8e00244841cd736e7d3dd2cd1ace72aaae8db375
Barış Bükümcüler <barisbukumculer@gmail.com>
2023-08-17 22:20:04 +0300

    service start
    new proje, React api

---
+++ src/pages/Home.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+import { getAllProducts } from '../Api'
+import { IProducts } from '../models/IProducts'
+
+ 
+
+function Home() {
+
+   const [proObj, setProObj]=useState<IProducts>()
+
+    useEffect(()=>{
+        getAllProducts().then(res=>{
+           //işlem başarılı, datalar geldi.
+           const dt=res.data
+           setProObj(dt)
+        }).catch(err=>{
+            //işlem başarısız
+            alert('Servis Hatası Oluştu')
+        })
+    },[])
+
+  return (
+    <>
+    
+    {proObj && proObj.products.map((item,index)=>
+
+        <div key={index}>
+            <div>{item.title} - {item.price}</div>
+            <div>{item.images[0]}</div>
+        </div>
+    )}
+    
+    </>
+  )
+}
+
+export default Home
